4adecd116c846f7bd96ab048ad82ef98c97a1c3a
johnbtweedie <johntweedie94@gmail.com>
2026-04-21 23:34:43 -0400

    refactor pull data from api script

---
+++ pull_data_db(deprecated).py
@@ -0,0 +1,292 @@
+import pandas as pd
+import numpy as np
+import json
+from nba_api.stats.endpoints import teamgamelogs, playergamelogs
+from nba_api.stats.static import teams
+import time
+import sqlite3
+from datetime import datetime
+import random
+
+def get_team_gamelog_data(team_id, n_games, season, max_retries=10):
+    headers = {
+        "Host": "stats.nba.com",
+        "Connection": "keep-alive",
+        "Accept": "application/json, text/plain, /",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)",
+        "Referer": "https://www.nba.com/",
+        "Origin": "https://www.nba.com",
+        "x-nba-stats-origin": "stats",
+        "x-nba-stats-token": "true",
+        "Accept-Language": "en-US,en;q=0.9"
+        }
+    
+    attempt = 0
+    while attempt < max_retries:
+        try:
+            team_gamelog_data = teamgamelogs.TeamGameLogs(team_id_nullable=team_id,
+                                                          season_nullable=season,
+                                                          last_n_games_nullable=n_games,
+                                                          timeout=1,
+                                                          headers=headers
+                                                          ).get_json()
+
+            data_dict = json.loads(team_gamelog_data)
+            df = pd.DataFrame(data_dict['resultSets'][0]['rowSet'],
+                              columns=data_dict['resultSets'][0]['headers'])
+
+            # df['GAME_DATE'] = pd.to_datetime(df['GAME_DATE'])
+            df['GAME_DATE'] = df['GAME_DATE'].str.split('T').str[0]
+            df['GAME_ID'] = df['GAME_ID'].astype('int')
+            df = df.set_index(['GAME_DATE', 'GAME_ID', 'TEAM_ABBREVIATION'])
+
+            print(f"...complete.")
+
+            return df
+        
+        except Exception as e:
+            print(f"Error: {e}. Retrying...")
+            attempt += 1
+            time.sleep(random.randint(1, 5))  # Wait for 5 seconds before retrying
+
+    print(f"Max retries ({max_retries}) reached. Couldn't fetch data.")
+    return None
+
+def get_player_gamelog_data(team_id, n_games, season, max_retries=10):
+    attempt = 0
+    while attempt < max_retries:
+        try:
+            team_gamelog_data = playergamelogs.PlayerGameLogs(team_id_nullable=team_id,
+                                                          season_nullable=season,
+                                                        #   last_n_games_nullable=n_games,
+                                                          timeout=1
+                                                          ).get_json()
+
+            data_dict = json.loads(team_gamelog_data)
+            df = pd.DataFrame(data_dict['resultSets'][0]['rowSet'],
+                              columns=data_dict['resultSets'][0]['headers'])
+
+            # df['GAME_DATE'] = pd.to_datetime(df['GAME_DATE'])
+            df['GAME_DATE'] = df['GAME_DATE'].str.split('T').str[0]
+            df['GAME_ID'] = df['GAME_ID'].astype('int')
+            df = df.set_index(['GAME_DATE', 'GAME_ID', 'TEAM_ABBREVIATION'])
+
+            return df
+        except Exception as e:
+            print(f"Error: {e}. Retrying...")
+            attempt += 1
+            time.sleep(random.randint(1, 5))  # Wait for 5 seconds before retrying
+
+    print(f"Max retries ({max_retries}) reached. Couldn't fetch data.")
+    return None
+
+def update_database():
+
+    # Create a connection to the SQLite database (or create it if it doesn't exist)
+    conn = sqlite3.connect('nba_database_2025-01-18.db')
+    teams_dict = teams.get_teams()
+
+    n_games = 82
+    seasons = [
+        '2012-13',
+        '2013-14',
+        '2014-15',
+        '2015-16',
+        '2016-17',
+        '2017-18',
+        '2018-19',
+        '2019-20',
+        '2020-21',
+        '2021-22',
+        '2022-23',
+        '2023-24',
+        '2024-25',
+        '2025-26',
+        '2026-27',
+        '2027-28',
+        '2028-29',
+        '2029-30'
+               ]
+
+    # grab the most recent game's date
+    query = """
+    SELECT MAX(GAME_DATE) as most_recent_date
+    FROM formatted_data_table
+    """
+    try:
+        most_recent_game_date = pd.read_sql(query, conn)
+
+        # get string of the most recent game's year and month
+        # use these to determine which seasons needs to be updated
+        year_last_updated = most_recent_game_date.iloc[0].str.split('-').str[0].iloc[0]
+        month_last_updated = most_recent_game_date.iloc[0].str.split('-').str[1].iloc[0]
+
+        if str(month_last_updated) == 'None':
+            month_last_updated = '1'
+
+        if str(year_last_updated) == 'None':
+            year_last_updated = seasons[1].split('-')[0]
+
+        if int(month_last_updated) >= 10:
+            season_last_updated = str(int(year_last_updated)) + '-' + str((int(year_last_updated) % 100) + 1)
+        else:
+            season_last_updated = str(int(year_last_updated) - 1) + '-' + str(int(year_last_updated) % 100)
+
+        position = seasons.index(season_last_updated)
+
+
+    except Exception as e:
+        # season_last_updated = '2012-13'
+        season_last_updated = seasons[0]
+        position = seasons.index(season_last_updated)
+
+    if datetime.now().month >= 10:
+        current_season = str(int(datetime.now().year)) + '-' + str((int(datetime.now().year) % 100) + 1)
+    else:
+        current_season = str(int(datetime.now().year) - 1) + '-' + str(int(datetime.now().year) % 100)
+
+    # update from the season last updated until the current season
+    df = pd.DataFrame()
+    for season in seasons[(seasons.index(season_last_updated)):(seasons.index(current_season)+1)]:
+        print('grabbing api data for ', season)
+        for i in range(0, len(teams_dict)):
+            # df = pd.concat([df, get_team_gamelog_data(i, n_games, season)], axis=0)
+            print(f"grabbing {teams_dict[i]['full_name']} {season} data...")
+            df = pd.concat([df, get_team_gamelog_data(teams_dict[i]['id'], n_games, season)], axis=0)
+
+    # factor encoding
+    print('processing api data...')
+    df['Home'] = df['MATCHUP'].apply(lambda x: 0 if '@' in x else 1)
+    df['Win'] = df['WL'].map({'W': 1, 'L': 0})
+
+    # calculate per-48 minute stats (corrects for overtime games)
+    cols = ['FGM', 'FGA', 'FG3M', 'FG3A', 'FTM',
+            'FTA', 'OREB', 'DREB', 'REB', 'AST', 'TOV', 'STL',
+            'BLK', 'BLKA', 'PF', 'PFD', 'PTS', 'PLUS_MINUS']
+
+    new_cols = []
+    for col in cols:
+        new_col_name = col + '_per48'
+        df[new_col_name] = df[col] / df['MIN'] * 48
+        new_cols.append(new_col_name)
+
+    df[new_cols] = np.round(df[new_cols], 2)
+
+    # match opponents to get stats against
+    df = match_opponent_stats(df)
+
+    # cols = ['Win', 'FGM_per48', 'FGA_per48',
+    #         'FG3M_per48', 'FG3A_per48',
+    #         'FTM_per48', 'FTA_per48',
+    #         'OREB_per48', 'DREB_per48',
+    #         'REB_per48', 'AST_per48', 'TOV_per48',
+    #         'STL_per48', 'BLK_per48', 'BLKA_per48',
+    #         'PF_per48', 'PFD_per48', 'PTS_per48',
+    #         'FG_PCT', 'FG3_PCT']
+
+    # # Stats against
+    # print('matching opponents for stats against...')
+    # total_games = len(df.index.get_level_values('GAME_ID').unique())
+    # interval = total_games / 10
+
+    # for i, game_id in enumerate(df.index.get_level_values('GAME_ID').unique()):
+    #     if i % int(interval) == 0:
+    #         print(f"Progress: {int((100 * i / total_games)+1)}%")
+
+    #     team_abvs = df.loc[:, game_id, :].index.get_level_values('TEAM_ABBREVIATION')
+    #     if len(team_abvs) < 2:
+    #         print('no opponent match')
+    #     else:
+    #         for col in cols:
+    #             col_name = col + '_against'
+
+    #             df.loc[(slice(None), game_id, team_abvs[0]),
+    #                    col_name] = df.loc[(slice(None),
+    #                                        game_id, team_abvs[1])][col].iloc[0]
+
+    #             df.loc[(slice(None), game_id, team_abvs[1]),
+    #                    col_name] = df.loc[(slice(None),
+    #                                        game_id, team_abvs[0])][col].iloc[0]
+
+    # print('saving processed data...')
+
+    try:
+        df_existing = pd.read_sql('SELECT * FROM formatted_data_table', conn)
+        # df_existing.set_index(['GAME_DATE', 'GAME_ID', 'TEAM_ABBREVIATION'])
+    except pd.io.sql.DatabaseError:
+        print('no existing data detected')
+        columns = df.reset_index().columns.to_list()
+        df_existing = pd.DataFrame(columns=columns)
+
+    df_existing['GAME_ID'] = df_existing['GAME_ID'].astype('int')
+    df_existing.set_index(['GAME_DATE', 'GAME_ID', 'TEAM_ABBREVIATION'], inplace=True)
+
+    if 'level_0' in df_existing.columns:
+        df_existing.drop('level_0', axis=1, inplace=True)
+    if 'index' in df_existing.columns:
+        df_existing.drop('index', axis=1, inplace=True)
+
+    combined_data = pd.concat([df_existing, df])
+    # combined_data.reset_index(inplace=True)
+    # combined_data.drop('index', axis=1, inplace=True)
+    
+    combined_data = combined_data[~combined_data.index.duplicated(keep='last')]
+    combined_data = combined_data.sort_index(level=['GAME_ID'])
+    # combined_data = combined_data.sort_values(by='GAME_ID')
+
+    print('saving to database...')
+    combined_data.to_sql('formatted_data_table', conn, if_exists='replace', index=True)
+    # combined_data.to_csv('historical_stats.csv')
+
+    # df_new = pd.read_sql('SELECT * FROM formatted_data_table', conn)
+    conn.close()
+
+def match_opponent_stats(df):
+    '''
+    match gamelogs for opponents in a game, for all games
+    returns original dataframe with stats against included in new columns
+    '''
+    df_reset = df.reset_index()
+
+    # self-merge on GAME_ID
+    merged = df_reset.merge(
+        df_reset,
+        on='GAME_ID',
+        suffixes=('', '_opp')
+    )
+
+    # remove self-join (team vs itself)
+    merged = merged[
+        merged['TEAM_ABBREVIATION'] != merged['TEAM_ABBREVIATION_opp']
+    ]
+
+    # keep only one opponent row per team
+    merged = merged.drop_duplicates(
+        subset=['GAME_ID', 'TEAM_ABBREVIATION']
+    )
+
+    # columns to copy
+    cols = ['Win', 'FGM_per48', 'FGA_per48',
+        'FG3M_per48', 'FG3A_per48',
+        'FTM_per48', 'FTA_per48',
+        'OREB_per48', 'DREB_per48',
+        'REB_per48', 'AST_per48', 'TOV_per48',
+        'STL_per48', 'BLK_per48', 'BLKA_per48',
+        'PF_per48', 'PFD_per48', 'PTS_per48',
+        'FG_PCT', 'FG3_PCT']
+
+    for col in cols:
+        merged[f"{col}_against"] = merged[f"{col}_opp"]
+
+    keep_cols = [
+        'GAME_DATE', 'GAME_ID', 'TEAM_ABBREVIATION'
+    ] + list(df.columns) + [f"{c}_against" for c in cols]
+
+    final = merged[keep_cols]
+
+    return final.set_index(['GAME_DATE', 'GAME_ID', 'TEAM_ABBREVIATION'])
+
+if __name__ == '__main__':
+    update_database()
+
+print('complete')
--- pull_data_db.py
+++ pull_data_db.py
@@ -1,157 +1,130 @@
 import pandas as pd
 import numpy as np
 import json
-from nba_api.stats.endpoints import teamgamelogs, playergamelogs
-from nba_api.stats.static import teams
-import time
 import sqlite3
-from datetime import datetime
+import time
 import random
+from datetime import datetime
+from nba_api.stats.endpoints import teamgamelogs
+from nba_api.stats.static import teams
 
-def get_team_gamelog_data(team_id, n_games, season, max_retries=10):
-    attempt = 0
-    while attempt < max_retries:
-        try:
-            team_gamelog_data = teamgamelogs.TeamGameLogs(team_id_nullable=team_id,
-                                                          season_nullable=season,
-                                                          last_n_games_nullable=n_games,
-                                                          timeout=1
-                                                          ).get_json()
 
-            data_dict = json.loads(team_gamelog_data)
-            df = pd.DataFrame(data_dict['resultSets'][0]['rowSet'],
-                              columns=data_dict['resultSets'][0]['headers'])
+class getData:
 
-            # df['GAME_DATE'] = pd.to_datetime(df['GAME_DATE'])
-            df['GAME_DATE'] = df['GAME_DATE'].str.split('T').str[0]
-            df['GAME_ID'] = df['GAME_ID'].astype('int')
-            df = df.set_index(['GAME_DATE', 'GAME_ID', 'TEAM_ABBREVIATION'])
+    def __init__(self, db_path='nba_database.db', n_games=82):
+        self.db_path = db_path
+        self.n_games = n_games
+        self.teams = teams.get_teams()
 
-            return df
-        except Exception as e:
-            print(f"Error: {e}. Retrying...")
-            attempt += 1
-            time.sleep(random.randint(1, 5))  # Wait for 5 seconds before retrying
+    def fetch_team_gamelog(self, team_id, season, max_retries=10):
 
-    print(f"Max retries ({max_retries}) reached. Couldn't fetch data.")
-    return None
+        headers = {
+            "Host": "stats.nba.com",
+            "Connection": "keep-alive",
+            "Accept": "application/json, text/plain, /",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)",
+            "Referer": "https://www.nba.com/",
+            "Origin": "https://www.nba.com",
+            "x-nba-stats-origin": "stats",
+            "x-nba-stats-token": "true",
+            "Accept-Language": "en-US,en;q=0.9"
+            }
+        for attempt in range(max_retries):
+            try:
+                response = teamgamelogs.TeamGameLogs(
+                    team_id_nullable=team_id,
+                    season_nullable=season,
+                    last_n_games_nullable=self.n_games,
+                    timeout=2,
+                    headers=headers
+                ).get_json()
 
-def get_player_gamelog_data(team_id, n_games, season, max_retries=10):
-    attempt = 0
-    while attempt < max_retries:
-        try:
-            team_gamelog_data = playergamelogs.PlayerGameLogs(team_id_nullable=team_id,
-                                                          season_nullable=season,
-                                                        #   last_n_games_nullable=n_games,
-                                                          timeout=1
-                                                          ).get_json()
+                data = json.loads(response)
+                df = pd.DataFrame(
+                    data['resultSets'][0]['rowSet'],
+                    columns=data['resultSets'][0]['headers']
+                )
 
-            data_dict = json.loads(team_gamelog_data)
-            df = pd.DataFrame(data_dict['resultSets'][0]['rowSet'],
-                              columns=data_dict['resultSets'][0]['headers'])
+                df['GAME_DATE'] = df['GAME_DATE'].str.split('T').str[0]
+                df['GAME_ID'] = df['GAME_ID'].astype(int)
 
-            # df['GAME_DATE'] = pd.to_datetime(df['GAME_DATE'])
-            df['GAME_DATE'] = df['GAME_DATE'].str.split('T').str[0]
-            df['GAME_ID'] = df['GAME_ID'].astype('int')
-            df = df.set_index(['GAME_DATE', 'GAME_ID', 'TEAM_ABBREVIATION'])
+                return df.set_index(['GAME_DATE', 'GAME_ID', 'TEAM_ABBREVIATION'])
 
-            return df
-        except Exception as e:
-            print(f"Error: {e}. Retrying...")
-            attempt += 1
-            time.sleep(random.randint(1, 5))  # Wait for 5 seconds before retrying
+            except Exception as e:
+                time.sleep(random.uniform(1, 3))
 
-    print(f"Max retries ({max_retries}) reached. Couldn't fetch data.")
-    return None
+        return None
 
-def update_database():
-
-    # Create a connection to the SQLite database (or create it if it doesn't exist)
-    conn = sqlite3.connect('nba_database_2025-01-18.db')
-    teams_dict = teams.get_teams()
-
-    n_games = 82
-    seasons = ['2012-13',
-               '2013-14',
-               '2014-15',
-               '2015-16',
-               '2016-17',
-               '2017-18',
-               '2018-19',
-               '2019-20',
-               '2020-21',
-               '2021-22',
-               '2022-23',
-               '2023-24',
-               '2024-25',
-               '2025-26',
-               '2026-27',
-               '2027-28',
-               '2028-29',
-               '2029-30']
-
-    # grab the most recent game's date
-    query = """
-    SELECT MAX(GAME_DATE) as most_recent_date
-    FROM formatted_data_table
-    """
-    try:
-        most_recent_game_date = pd.read_sql(query, conn)
-
-        # get string of the most recent game's year and month
-        # use these to determine which seasons needs to be updated
-        year_last_updated = most_recent_game_date.iloc[0].str.split('-').str[0].iloc[0]
-        month_last_updated = most_recent_game_date.iloc[0].str.split('-').str[1].iloc[0]
-
-        if str(month_last_updated) == 'None':
-            month_last_updated = '1'
-
-        if str(year_last_updated) == 'None':
-            year_last_updated = seasons[1].split('-')[0]
-
-        if int(month_last_updated) >= 10:
-            season_last_updated = str(int(year_last_updated)) + '-' + str((int(year_last_updated) % 100) + 1)
+    def get_current_season(self):
+        now = datetime.now()
+        if now.month >= 10:
+            return f"{now.year}-{(now.year % 100) + 1}"
         else:
-            season_last_updated = str(int(year_last_updated) - 1) + '-' + str(int(year_last_updated) % 100)
+            return f"{now.year - 1}-{now.year % 100}"
 
-        position = seasons.index(season_last_updated)
+    def get_last_updated_season(self, conn, seasons):
+        '''
+        open the formatted data table and find the most recent date
+        return this date to help specify which season's stats to grab from the api
+        '''
+        try:
+            df = pd.read_sql("SELECT MAX(GAME_DATE) as d FROM formatted_data_table", conn)
+            date = df.iloc[0, 0]
 
-    except Exception as e:
-        # season_last_updated = '2012-13'
-        season_last_updated = seasons[0]
-        position = seasons.index(season_last_updated)
+            if pd.isna(date):
+                return seasons[0]
 
-    if datetime.now().month >= 10:
-        current_season = str(int(datetime.now().year)) + '-' + str((int(datetime.now().year) % 100) + 1)
-    else:
-        current_season = str(int(datetime.now().year) - 1) + '-' + str(int(datetime.now().year) % 100)
+            year, month, *_ = date.split('-')
 
-    # update from the season last updated until the current season
-    df = pd.DataFrame()
-    for season in seasons[(seasons.index(season_last_updated)):(seasons.index(current_season)+1)]:
-        print('grabbing api data for ', season)
-        for i in range(0, len(teams_dict)):
-            df = pd.concat([df, get_team_gamelog_data(i, n_games, season)], axis=0)
+            # nba season starts in october (month 10)
+            if int(month) >= 10:
+                return f"{year}-{(int(year) % 100) + 1}"
+            else:
+                return f"{int(year) - 1}-{int(year) % 100}"
 
-    # factor encoding
-    print('processing api data...')
-    df['Home'] = df['MATCHUP'].apply(lambda x: 0 if '@' in x else 1)
-    df['Win'] = df['WL'].map({'W': 1, 'L': 0})
+        except:
+            return seasons[0]
 
-    # calculate per-48 minute stats (corrects for overtime games)
-    cols = ['FGM', 'FGA', 'FG3M', 'FG3A', 'FTM',
-            'FTA', 'OREB', 'DREB', 'REB', 'AST', 'TOV', 'STL',
-            'BLK', 'BLKA', 'PF', 'PFD', 'PTS', 'PLUS_MINUS']
+    def add_features(self, df):
 
-    new_cols = []
-    for col in cols:
-        new_col_name = col + '_per48'
-        df[new_col_name] = df[col] / df['MIN'] * 48
-        new_cols.append(new_col_name)
+        df['Home'] = df['MATCHUP'].str.contains('@').map({True: 0, False: 1})
+        df['Win'] = df['WL'].map({'W': 1, 'L': 0})
 
-    df[new_cols] = np.round(df[new_cols], 2)
+        stats = [
+            'FGM', 'FGA', 'FG3M', 'FG3A', 'FTM', 'FTA',
+            'OREB', 'DREB', 'REB', 'AST', 'TOV',
+            'STL', 'BLK', 'BLKA', 'PF', 'PFD', 'PTS'
+        ]
 
-    cols = ['Win', 'FGM_per48', 'FGA_per48',
+        # normalize relevant stats to per 48-minutes to correct for overtime games
+        for col in stats:
+            df[f"{col}_per48"] = (df[col] / df['MIN'] * 48).round(2)
+
+        return df
+
+    def add_opponent_stats(self, df):
+
+        df_reset = df.reset_index()
+
+        # self-merge on GAME_ID
+        merged = df_reset.merge(
+            df_reset,
+            on='GAME_ID',
+            suffixes=('', '_opp')
+        )
+
+        # remove self-join (team vs itself)
+        merged = merged[
+            merged['TEAM_ABBREVIATION'] != merged['TEAM_ABBREVIATION_opp']
+        ]
+
+        # keep only one opponent row per team
+        merged = merged.drop_duplicates(
+            subset=['GAME_ID', 'TEAM_ABBREVIATION']
+        )
+
+        # relevant opponents stat columns to copy
+        cols = ['Win', 'FGM_per48', 'FGA_per48',
             'FG3M_per48', 'FG3A_per48',
             'FTM_per48', 'FTA_per48',
             'OREB_per48', 'DREB_per48',
@@ -160,66 +133,74 @@ def update_database():
             'PF_per48', 'PFD_per48', 'PTS_per48',
             'FG_PCT', 'FG3_PCT']
 
-    # Stats against
-    print('matching opponents for stats against...')
-    total_games = len(df.index.get_level_values('GAME_ID').unique())
-    interval = total_games / 10
+        for col in cols:
+            merged[f"{col}_against"] = merged[f"{col}_opp"]
 
-    for i, game_id in enumerate(df.index.get_level_values('GAME_ID').unique()):
-        if i % int(interval) == 0:
-            print(f"Progress: {int(100 * i / total_games)}%")
+        keep_cols = [
+            'GAME_DATE', 'GAME_ID', 'TEAM_ABBREVIATION'
+        ] + list(df.columns) + [f"{c}_against" for c in cols]
 
-        team_abvs = df.loc[:, game_id, :].index.get_level_values('TEAM_ABBREVIATION')
-        if len(team_abvs) < 2:
-            print('no opponent match')
-        else:
-            for col in cols:
-                col_name = col + '_against'
+        final = merged[keep_cols]
 
-                df.loc[(slice(None), game_id, team_abvs[0]),
-                       col_name] = df.loc[(slice(None),
-                                           game_id, team_abvs[1])][col].iloc[0]
+        return final.set_index(['GAME_DATE', 'GAME_ID', 'TEAM_ABBREVIATION'])
 
-                df.loc[(slice(None), game_id, team_abvs[1]),
-                       col_name] = df.loc[(slice(None),
-                                           game_id, team_abvs[0])][col].iloc[0]
+    def run(self):
 
-    print('saving processed data...')
+        conn = sqlite3.connect(self.db_path)
 
-    try:
-        df_existing = pd.read_sql('SELECT * FROM formatted_data_table', conn)
-        # df_existing.set_index(['GAME_DATE', 'GAME_ID', 'TEAM_ABBREVIATION'])
-    except pd.io.sql.DatabaseError:
-        print('no existing data detected')
-        columns = df.reset_index().columns.to_list()
-        df_existing = pd.DataFrame(columns=columns)
+        seasons = [f"{y}-{(y % 100) + 1}" for y in range(2022, 2030)]
 
-    df_existing['GAME_ID'] = df_existing['GAME_ID'].astype('int')
-    df_existing.set_index(['GAME_DATE', 'GAME_ID', 'TEAM_ABBREVIATION'], inplace=True)
+        # construct indicies: grab the most recent season present from the database and get the current season based on today's date
+        last_season = self.get_last_updated_season(conn, seasons)
+        current_season = self.get_current_season()
+        start_idx = seasons.index(last_season)
+        end_idx = seasons.index(current_season)
 
-    if 'level_0' in df_existing.columns:
-        df_existing.drop('level_0', axis=1, inplace=True)
-    if 'index' in df_existing.columns:
-        df_existing.drop('index', axis=1, inplace=True)
+        df_all = []
 
-    combined_data = pd.concat([df_existing, df])
-    # combined_data.reset_index(inplace=True)
-    # combined_data.drop('index', axis=1, inplace=True)
-    #
-    combined_data = combined_data[~combined_data.index.duplicated(keep='last')]
-    combined_data = combined_data.sort_index(level=['GAME_ID'])
-    # combined_data = combined_data.sort_values(by='GAME_ID')
+        # loop through all specified seasons to grab data season by season
+        for season in seasons[start_idx:end_idx + 1]:
+            print("\n")
+            print("="*50)
+            print(f"Fetching {season}")
+            print("="*50)
+            # loop through teams list and fetch their season's gamelogs
+            i = 0
+            for team in self.teams:
+                print(f"Fetching {team['full_name']}...")
+                df = self.fetch_team_gamelog(team['id'], season)
+                if df is not None:
+                    i += 1
+                    df_all.append(df)
+                    print("...complete")
+                else:
+                    print(f"Data aquisition failed for {team['full_name']}.")
+            print("="*50)
+            print(f"{season} complete. Data for {i} teams obtained successfully.")
+            print("="*50)
 
+        df = pd.concat(df_all)
 
+        print("\nProcessing features...")
+        df = self.add_features(df)
+        print("...complete")
 
-    print('saving to database...')
-    combined_data.to_sql('formatted_data_table', conn, if_exists='replace', index=True)
-    # combined_data.to_csv('historical_stats.csv')
+        print("\nMatching opponents...")
+        df = self.add_opponent_stats(df)
+        print("...complete")
 
-    # df_new = pd.read_sql('SELECT * FROM formatted_data_table', conn)
-    conn.close()
+        print("\nSaving to DB...")
+        df.to_sql('formatted_data_table', conn, if_exists='replace')
+        print("...complete")
+
+        conn.close()
+        print("\n")
+        print("="*50)
+        print("="*50)
+        print("Data collection script complete.")
+        print("="*50)
+        print("="*50)
 
 if __name__ == '__main__':
-    update_database()
-
-print('complete')
+    data_getter = getData()
+    data_getter.run()
